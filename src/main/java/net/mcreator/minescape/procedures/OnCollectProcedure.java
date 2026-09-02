c745ca391e9ae319b28e97019cab9d96e6c1d1f8
catherine <tfem_catherine@proton.me>
2026-09-02 06:51:20 -0400

    Update OnCollect procedure and mod file

--- src/main/java/net/mcreator/minescape/procedures/OnCollectProcedure.java
+++ src/main/java/net/mcreator/minescape/procedures/OnCollectProcedure.java
@@ -6,7 +6,9 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.util.RandomSource;
 import net.minecraft.util.Mth;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.resources.Identifier;
+import net.minecraft.network.chat.Component;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.BlockPos;
 
@@ -19,9 +21,9 @@ public class OnCollectProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
 		if (entity == null)
 			return;
+		if (!entity.level().isClientSide())
+			entity.discard();
 		if (entity instanceof GoldGiftEntity) {
-			if (!entity.level().isClientSide())
-				entity.discard();
 			MinescapeMod.LOGGER.info("Gold Gift collected");
 			MinescapeModVariables.WorldVariables.get(world).GoldGifts = MinescapeModVariables.WorldVariables.get(world).GoldGifts + MinescapeModVariables.WorldVariables.get(world).GoldGiftMultiplier * 1;
 			MinescapeModVariables.WorldVariables.get(world).markSyncDirty();
@@ -32,9 +34,10 @@ public class OnCollectProcedure {
 					_level.playLocalSound(x, y, z, BuiltInRegistries.SOUND_EVENT.getValue(Identifier.parse("minescape:goldcollect")), SoundSource.MASTER, (float) 0.2, (float) Mth.nextDouble(RandomSource.create(), 0.99, 1.07), false);
 				}
 			}
+			if (world instanceof ServerLevel _level) {
+				_level.getServer().getPlayerList().broadcastSystemMessage(Component.literal(("GoldGifts: " + MinescapeModVariables.WorldVariables.get(world).GoldGifts)), false);
+			}
 		} else if (entity instanceof NullGiftEntity) {
-			if (!entity.level().isClientSide())
-				entity.discard();
 			MinescapeMod.LOGGER.info("Gift collected");
 			MinescapeModVariables.WorldVariables.get(world).GiftsCollected = MinescapeModVariables.WorldVariables.get(world).GiftsCollected + 1;
 			MinescapeModVariables.WorldVariables.get(world).markSyncDirty();
